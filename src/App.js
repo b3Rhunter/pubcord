50f5a252dd5c86e1ad113c437bb00d3e53454691
Pub <96320073+b3Rhunter@users.noreply.github.com>
2023-03-07 23:01:34 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,12 +17,7 @@ const contractAddress = '0x91C39EA1c3110473738094536338C270d5F3A621'
 
 
 // Socket
-const socket = io("https://pubcord-server.onrender.com", {
-  withCredentials: true,
-  extraHeaders: {
-    "my-custom-header": "abcd"
-  }
-});
+const socket = io("https://pubcord-server.onrender.com");
 
 function App() {
   const [provider, setProvider] = useState(null)
